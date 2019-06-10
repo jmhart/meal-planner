6f5178ce7c718a8263295a35d220f3862defe677
Jeff Hart <jeffhart91@gmail.com>
2019-06-10 13:32:20 -0500

    Added navigation skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { memo } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import MealForm from "./components/MealForm";
 
 const App = memo(props => {
   return (
-    <Layout>
-      <MealForm />
-    </Layout>
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
+        <Layout>
+          <Route path="/" exact />
+          <Route path="/meals/create" exact component={MealForm} />
+        </Layout>
+      </Router>
+    </React.Fragment>
   );
 });
 
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,18 +1,52 @@
 import React, { memo } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography, Paper } from "@material-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
 
-const Layout = memo(props => (
-  <Paper
-    elevation={0}
-    style={{ padding: 0, margin: 0, backgroundColor: "#fafafa" }}
-  >
-    <AppBar color="primary" position="static" style={{ height: 64 }}>
-      <Toolbar style={{ height: 64 }}>
-        <Typography color="inherit">Meal Planner</Typography>
-      </Toolbar>
-    </AppBar>
-    {props.children}
-  </Paper>
-));
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    flexWrap: "wrap"
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+    color: "#fff"
+  }
+}));
+
+const Layout = props => {
+  const classes = useStyles();
+  return (
+    <Paper>
+      <AppBar color="primary" position="static" style={{ height: 64 }}>
+        <Toolbar className={classes.toolbar} style={{ height: 64 }}>
+          <Typography>
+            <Link className={classes.link} component={RouterLink} to="/">
+              Home
+            </Link>
+            <Link className={classes.link} component={RouterLink} to="/meals">
+              Meals
+            </Link>
+            <Link
+              className={classes.link}
+              component={RouterLink}
+              to="/meals/create"
+            >
+              Create meal
+            </Link>
+            <Link
+              className={classes.link}
+              component={RouterLink}
+              to="/ingredients"
+            >
+              Ingredients
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {props.children}
+    </Paper>
+  );
+};
 
 export default Layout;
